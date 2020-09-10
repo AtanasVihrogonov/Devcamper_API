02371748b6c1e20be70a0852c9b9cb78cf0437c4
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-09-10 16:04:42 +0100

    Aggregate - Calculating the avg course cost

--- models/course.js
+++ models/course.js
@@ -38,4 +38,39 @@ const CourseSchema = new mongoose.Schema({
   }
 });
 
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAvaregeCost = async function(bootcampId) {
+  console.log('Calculating avg cost...'.blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        avarageCost: { $avg: '$tuition' }
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      avarageCost: Math.ceil(obj[0].avarageCost / 10) * 10
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Call getAvarageCost after save
+CourseSchema.post('save', function() {
+  this.constructor.getAvaregeCost(this.bootcamp);
+});
+
+// Call getAvarageCost before remove
+CourseSchema.pre('remove', function() {
+  this.constructor.getAvaregeCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
