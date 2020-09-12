ad2f327e1ddb60c0474e63644572fa7be4cf5e34
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-09-12 17:35:00 +0100

    Register Encripting Passwords & Get JSON Web Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,5 +7,19 @@ const Exports = require('node-geocoder');
 // @route  GET /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async(req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
 });
