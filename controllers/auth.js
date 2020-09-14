66f4d341352aec9afa73cf130f8b3843b05d45a4
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-09-14 13:12:20 +0100

    Sending JWT in a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 // @desc   Register user
 // @route  POST /api/v1/auth/register
 // @access  Public
-exports.register = asyncHandler(async(req, res, next) => {
+exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
   // Create user
@@ -13,42 +13,61 @@ exports.register = asyncHandler(async(req, res, next) => {
     name,
     email,
     password,
-    role
+    role,
   });
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 // @desc   Login user
 // @route  POST /api/v1/auth/login
 // @access  Public
-exports.login = asyncHandler(async(req, res, next) => {
+exports.login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
 
   // Validate email and password
-  if(!email || !password) {
+  if (!email || !password) {
     return next(new ErrorResponse('Please provide an email and password', 400));
   }
 
   // Check for user
   const user = await User.findOne({ email }).select('+password');
 
-  if(!user) {
+  if (!user) {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
   // Check if password matches
   const isMatch = await user.matchPassword(password);
 
-  if(!isMatch) {
+  if (!isMatch) {
     return next(new ErrorResponse('Invalid credentials', 401));
   }
 
+  sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({ success: true, token });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if(process.emv.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+   .status(statusCode)
+   .cookie('token', token, options)
+   .json({
+    success: true,
+    token,
+  });
+};
