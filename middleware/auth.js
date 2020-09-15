df8fb4533a2d25957c067fa3ddff9e20be5b8d17
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-09-15 10:48:56 +0100

    Bootcamp & User Relationship

--- middleware/auth.js
+++ middleware/auth.js
@@ -38,9 +38,9 @@ exports.protect = asyncHandler(async (req, res, next) => {
 });
 
 // Grant access to specific roles
-exports.authorize = (...roles) => {
+exports.authorize = ( ...roles ) => {
   return (req, res, next) => {
-    if (!roles.includes(req.user.rule)) {
+    if (!roles.includes(req.user.role)) {
       return next(
         new ErrorResponse(
           `User role ${req.user.role} is not authorized to access this route`,
