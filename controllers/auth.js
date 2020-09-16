35246f3b3109273348ad813d61b504c22adba005
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-09-16 14:42:17 +0100

    Forgot Password - Generate Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -19,6 +19,31 @@ exports.register = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if(process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+   .status(statusCode)
+   .cookie('token', token, options)
+   .json({
+    success: true,
+    token,
+  });
+};
+
 // @desc   Login user
 // @route  POST /api/v1/auth/login
 // @access  Public
@@ -47,36 +72,32 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
-// Get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-
-  if(process.env.NODE_ENV === 'production') {
-    options.secure = true;
-  }
-
-  res
-   .status(statusCode)
-   .cookie('token', token, options)
-   .json({
-    success: true,
-    token,
-  });
-};
-
 // @desc   Get current logged in user
 // @route  POST /api/v1/auth/me
 // @access  Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+});
+
+// @desc  Forgot password
+// @route  POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if(!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false })
 
   res.status(200).json({
     success: true,
