a017484f5ed4b79886cae3cf5842c217ba8fb903
Atanas Vihrogonov <vihrogonov@protonmail.com>
2020-09-18 11:20:53 +0100

    Aggregate - Calculate Average Raiting

--- models/course.js
+++ models/course.js
@@ -44,7 +44,7 @@ const CourseSchema = new mongoose.Schema({
 });
 
 // Static method to get avg of course tuitions
-CourseSchema.statics.getAvaregeCost = async function(bootcampId) {
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
   const obj = await this.aggregate([
     {
       $match: { bootcamp: bootcampId }
@@ -52,28 +52,28 @@ CourseSchema.statics.getAvaregeCost = async function(bootcampId) {
     {
       $group: {
         _id: '$bootcamp',
-        avarageCost: { $avg: '$tuition' }
+        averageCost: { $avg: '$tuition' }
       }
     }
   ]);
 
   try {
     await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
-      avarageCost: Math.ceil(obj[0].avarageCost / 10) * 10
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
     });
   } catch (err) {
     console.error(err);
   }
 }
 
-// Call getAvarageCost after save
+// Call getAverageCost after save
 CourseSchema.post('save', function() {
-  this.constructor.getAvaregeCost(this.bootcamp);
+  this.constructor.getAverageCost(this.bootcamp);
 });
 
-// Call getAvarageCost before remove
+// Call getAverageCost before remove
 CourseSchema.pre('remove', function() {
-  this.constructor.getAvaregeCost(this.bootcamp);
+  this.constructor.getAverageCost(this.bootcamp);
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
